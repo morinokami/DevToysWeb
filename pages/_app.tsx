c1d137f76df233611a9eac2c8a3fbd57bfd2bedf
Shinya Fujino <shf0811@gmail.com>
2022-02-05 00:57:40 +0900

    Remove defaultTheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import SidebarLayout from "../layouts/SidebarLayout";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
+    <ThemeProvider attribute="class">
       <SidebarLayout>
         <Component {...pageProps} />
       </SidebarLayout>
