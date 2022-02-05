6f9305f244a93e9e39194eee38214fb55fa9f832
Shinya Fujino <shf0811@gmail.com>
2022-02-06 01:10:50 +0900

    GitHub link

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.VFC<HeaderProps> = ({ setNavIsOpen }) => {
   return (
     <div className="sticky top-0 z-20 flex h-8 w-full items-center bg-pink-500 px-4">
       <nav className="flex w-full flex-row justify-between">
-        <div className="flex align-middle">
+        <div className="flex items-center">
           <button className="mr-2 lg:hidden" onClick={() => setNavIsOpen(true)}>
             <FaBars />
           </button>
@@ -17,9 +17,13 @@ const Header: React.VFC<HeaderProps> = ({ setNavIsOpen }) => {
             <a>DevToysWeb</a>
           </Link>
         </div>
-        <button>
-          <FaGithub />
-        </button>
+        <div className="flex items-center">
+          <Link href="https://github.com/morinokami/DevToysWeb">
+            <a>
+              <FaGithub />
+            </a>
+          </Link>
+        </div>
       </nav>
     </div>
   );
