488ea55acb0d6d5234e01bfe15de9088849b6678
Shinya Fujino <shf0811@gmail.com>
2022-02-05 13:23:35 +0900

    wip

--- pages/[category]/index.tsx
+++ pages/[category]/index.tsx
@@ -1,10 +1,25 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
+
+import { nav } from "../../data/nav";
 
 const Category: NextPage = () => {
   const router = useRouter();
-  const { category } = router.query;
-  return <div>[{category}]</div>;
+  const path = router.asPath;
+  const navItems = nav.find(({ href }) => href === path)?.items ?? [];
+
+  return (
+    <>
+      {navItems.map(({ title, href }) => {
+        return (
+          <div key={title}>
+            <Link href={href}>{title}</Link>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Category;
