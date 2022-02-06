8b4195462459250eaa3cd144f1dd56180eb11300
Shinya Fujino <shf0811@gmail.com>
2022-02-06 23:50:10 +0900

    Add space

--- layouts/CardLayout.tsx
+++ layouts/CardLayout.tsx
@@ -1,4 +1,5 @@
 import NavCard from "../components/NavCard";
+import Spacer from "../components/Spacer";
 import { NavItem } from "../data/nav";
 
 interface CardLayoutProps {
@@ -7,18 +8,26 @@ interface CardLayoutProps {
 
 const CardLayout: React.VFC<CardLayoutProps> = ({ navItems }) => {
   return (
-    <div className="sm:grid sm:grid-cols-3 sm:gap-y-4 md:flex md:flex-wrap md:justify-start xs:grid-cols-2">
-      {navItems.map(({ title, href, desc }) => {
-        return (
-          <div
-            key={title}
-            className="sm:flex sm:justify-center md:my-3 md:ml-0 md:mr-4"
-          >
-            <NavCard title={title} href={href} desc={desc} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="md:hidden">
+        <Spacer y={12} />
+      </div>
+      <div className="sm:grid sm:grid-cols-3 sm:gap-y-4 md:flex md:flex-wrap md:justify-start xs:grid-cols-2">
+        {navItems.map(({ title, href, desc }) => {
+          return (
+            <div
+              key={title}
+              className="sm:flex sm:justify-center md:my-3 md:ml-0 md:mr-4"
+            >
+              <NavCard title={title} href={href} desc={desc} />
+            </div>
+          );
+        })}
+      </div>
+      <div className="md:hidden">
+        <Spacer y={24} />
+      </div>
+    </>
   );
 };
 
