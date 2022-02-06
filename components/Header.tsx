da89da38a6fa99df4e2b2512795fad7f7db1ad95
Shinya Fujino <shf0811@gmail.com>
2022-02-06 17:30:02 +0900

    Adjust text

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,21 +7,19 @@ interface HeaderProps {
 
 const Header: React.VFC<HeaderProps> = ({ setNavIsOpen }) => {
   return (
-    <div className="sticky top-0 z-20 flex h-8 w-full items-center bg-gray-100 px-4 dark:bg-dark-5">
-      <nav className="flex w-full flex-row justify-between">
-        <div className="flex items-center">
+    <div className="sticky top-0 z-20 h-12 w-full bg-gray-100 px-4 dark:bg-dark-5">
+      <nav className="flex h-full w-full items-center justify-between">
+        <div className="flex">
           <button className="mr-2 lg:hidden" onClick={() => setNavIsOpen(true)}>
             <FaBars />
           </button>
-          <div>DevToysWeb</div>
+          <div className="translate-y-0.5">DevToysWeb</div>
         </div>
-        <div className="flex items-center">
-          <Link href="https://github.com/morinokami/DevToysWeb">
-            <a>
-              <FaGithub />
-            </a>
-          </Link>
-        </div>
+        <Link href="https://github.com/morinokami/DevToysWeb">
+          <a>
+            <FaGithub />
+          </a>
+        </Link>
       </nav>
     </div>
   );
