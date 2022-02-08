e2a057af9b88e269807cc81c156e97604dd86289
Shinya Fujino <shf0811@gmail.com>
2022-02-09 00:25:12 +0900

    wip

--- pages/[category]/number-base.tsx
+++ pages/[category]/number-base.tsx
@@ -1,4 +1,6 @@
+import { Listbox, Switch, Transition } from "@headlessui/react";
 import { NextPage } from "next";
+import React from "react";
 import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
@@ -9,8 +11,17 @@ import Spacer from "../../components/Spacer";
 import { IconBeerMini, IconCopy } from "../../data/icon";
 import MainLayout from "../../layouts/MainLayout";
 
+const bases = [
+  { name: "Octal" },
+  { name: "Binary" },
+  { name: "Decimal" },
+  { name: "Hexadecimal" },
+];
+
 const NumberBase: NextPage = () => {
   const [input, setInput] = useState("");
+  const [format, setFormat] = useState(true);
+  const [base, setBase] = useState(bases[2]);
 
   const decimal = Number(input);
   const hex = Number(input).toString(16);
@@ -22,14 +33,79 @@ const NumberBase: NextPage = () => {
       <div>
         <SectionHeader title="Configuration" />
         <Configuration icon={IconBeerMini} title="Format number">
-          On
+          <Switch
+            checked={format}
+            onChange={setFormat}
+            className={`${
+              format ? "bg-blue-500" : "border-2 bg-dark-5"
+            } relative inline-flex h-6 w-11 items-center rounded-full transition-colors duration-200 ease-in-out`}
+          >
+            <span className="sr-only">Format the result</span>
+            <span
+              aria-hidden="true"
+              className={`${
+                format ? "translate-x-7" : "translate-x-1"
+              } inline-block h-3 w-3 transform rounded-full bg-white transition duration-200 ease-in-out`}
+            />
+          </Switch>
         </Configuration>
         <Configuration
           icon={IconBeerMini}
           title="Input type"
           subtitle="Select which Input type you want to use"
         >
-          Select
+          <Listbox value={base} onChange={setBase}>
+            <div className="relative mt-1">
+              <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                <span className="block truncate">{base.name}</span>
+              </Listbox.Button>
+              <Transition
+                as={React.Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {bases.map((base, baseIdx) => (
+                    <Listbox.Option
+                      key={baseIdx}
+                      className={({ active }) =>
+                        `${
+                          active
+                            ? "bg-amber-100 text-amber-900"
+                            : "text-gray-900"
+                        }
+                          relative cursor-default select-none py-2 pl-10 pr-4`
+                      }
+                      value={base}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <span
+                            className={`${
+                              selected ? "font-medium" : "font-normal"
+                            } block truncate`}
+                          >
+                            {base.name}
+                          </span>
+                          {selected ? (
+                            <span
+                              className={`${
+                                active ? "text-amber-600" : "text-amber-600"
+                              }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                            >
+                              x
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </Listbox>
         </Configuration>
       </div>
       <div>
