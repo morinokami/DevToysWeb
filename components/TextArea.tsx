066e3c179c46b6e3033eca0b78d4371faf1557a4
Shinya Fujino <shf0811@gmail.com>
2022-02-12 17:12:17 +0900

    Make TextArea resizable

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -8,7 +8,7 @@ const TextArea: React.VFC<TextAreaProps> = ({ value, onChange, rows }) => {
   return (
     <div className="flex items-center">
       <textarea
-        className="grow resize-none rounded border border-gray-200 border-b-gray-400 bg-light-10 px-3 pt-[0.3rem] pb-[0.2rem] focus:border-b-blue-500 focus:outline-none dark:border-x-0 dark:border-t-0 dark:border-b-2 dark:border-gray-500 dark:bg-dark-30 focus:dark:border-blue-500 focus:dark:bg-dark-50"
+        className="grow rounded border border-gray-200 border-b-gray-400 bg-light-10 px-3 pt-[0.3rem] pb-[0.2rem] focus:border-b-blue-500 focus:outline-none dark:border-x-0 dark:border-t-0 dark:border-b-2 dark:border-gray-500 dark:bg-dark-30 focus:dark:border-blue-500 focus:dark:bg-dark-50"
         rows={rows ?? 10}
         value={value}
         onChange={(e) => (onChange ? onChange(e.target.value) : null)}
