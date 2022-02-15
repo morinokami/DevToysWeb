36a9f9577d3d80bbd8f9211de4b70e286b1b7d24
Shinya Fujino <shf0811@gmail.com>
2022-02-16 01:29:14 +0900

    Add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   i18n: {
     locales: ["en", "ja"],
     defaultLocale: "en",
   },
-};
+  pwa: {
+    dest: "public",
+  },
+});
 
 module.exports = nextConfig;
