010728f3b63dc8c2d8ca7c416c9dbab4b1b5bdc0
Shinya Fujino <shf0811@gmail.com>
2022-02-18 22:44:19 +0900

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {};
+
+module.exports = createJestConfig(customJestConfig);
