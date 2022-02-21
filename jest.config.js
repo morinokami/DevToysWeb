e79b182252f314bb45829e057190def2a98bbe50
Shinya Fujino <shf0811@gmail.com>
2022-02-22 00:12:02 +0900

    gzip

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["./jest.setup.js"],
 };
 
 module.exports = createJestConfig(customJestConfig);
