66f26b3645a8da8799ba236b165656c4d1f86f53
Shinya Fujino <shf0811@gmail.com>
2022-02-23 19:44:15 +0900

    Update Configuration

--- components/section/Configuration.tsx
+++ components/section/Configuration.tsx
@@ -11,13 +11,13 @@ const Configuration: React.FC<ConfigurationProps> = ({
   children,
 }) => {
   return (
-    <div className="flex h-16 items-center rounded border border-gray-200 bg-light-10 px-4 dark:border-none dark:bg-dark-30">
+    <div className="flex h-16 items-center rounded border border-light-40 bg-light-10 px-4 dark:border-none dark:bg-dark-30">
       <div className="pr-4">
         <Icon />
       </div>
-      <div className="ml-2 grow">
+      <div className="mx-2 grow">
         <div className="text-sm">{title}</div>
-        {subtitle && <div className="text-xs text-gray-400">{subtitle}</div>}
+        {subtitle && <div className="text-xs text-dark-10">{subtitle}</div>}
       </div>
       <div>{children}</div>
     </div>
