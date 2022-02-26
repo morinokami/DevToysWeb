d6e8a745f1c8bb7e19be03b37b6d6cba583b69bd
Shinya Fujino <shf0811@gmail.com>
2022-02-26 15:52:14 +0900

    Update Toggle color

--- components/io/Toggle.tsx
+++ components/io/Toggle.tsx
@@ -27,15 +27,19 @@ const Toggle: React.VFC<ToggleProps> = ({
         checked={on}
         onChange={onChange}
         className={`${
-          on ? "bg-blue-30" : "border border-dark-10 bg-dark-50"
+          on
+            ? "bg-blue-30"
+            : "border border-dark-10 bg-light-20 dark:bg-dark-50"
         } relative inline-flex h-6 w-11 items-center rounded-full transition-colors duration-200 ease-in-out`}
       >
         <span className="sr-only">{desc}</span>
         <span
           aria-hidden="true"
           className={`${
-            on ? "translate-x-7" : "translate-x-1"
-          } inline-block h-3 w-3 transform rounded-full bg-light-40 transition duration-200 ease-in-out`}
+            on
+              ? "translate-x-7 bg-light-10 dark:bg-dark-50"
+              : "translate-x-1 bg-dark-10 dark:bg-light-40"
+          } inline-block h-3 w-3 transform rounded-full transition duration-200 ease-in-out`}
         />
       </Switch>
     </div>
